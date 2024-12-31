2d4a1c401d5c5746bea5dde8fa65a6a11c990e7c
Joseph Hayes <joseph.hayes003@gmail.com>
2024-12-31 01:15:41 +0000

    shop

--- printful-proxy/api/products.js
+++ printful-proxy/api/products.js
@@ -1,6 +1,11 @@
-const PRINTFUL_API_KEY = process.env.PRINTFUL_API_KEY;
-
 export default async function handler(req, res) {
+    const PRINTFUL_API_KEY = process.env.PRINTFUL_API_KEY;
+
+    // Add CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allows requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Allows only GET requests
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allows Content-Type header
+
     try {
         const response = await fetch('https://api.printful.com/store/products', {
             headers: {
